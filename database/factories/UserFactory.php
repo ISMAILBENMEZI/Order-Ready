f07695cda870374c254e4b871a871ebd64b8530f
unknown <ismailbenmezi300@gmail.com>
2026-04-23 18:05:54 +0200

    fix problem factory in userFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'Ismail',
+            'email' => "admin@gmail.com",
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'role_id'    => \App\Models\Role::inRandomOrder()->first()->id,
