9347ead4cf6d0a97df69b01ef2f74f4bb1fec831
unknown <ismailbenmezi300@gmail.com>
2026-04-23 14:55:15 +0200

    fix error migration in table supscrip

--- database/migrations/2026_02_18_032805_create_subscriptions_table.php
+++ database/migrations/2026_02_18_032805_create_subscriptions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('saller_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('paln_id')->constrained('palns')->onDelete('restrict');
+            $table->foreignId('plan_id')->constrained('plans')->onDelete('restrict');
 
             $table->date('start_date');
             $table->date('end_date');
