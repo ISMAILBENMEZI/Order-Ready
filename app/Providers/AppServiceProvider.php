3025a7cfcefbec666b028d152083be558c457957
unknown <ismailbenmezi300@gmail.com>
2026-04-24 13:18:23 +0200

    add knew condiation in method boot for fix url path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,16 @@ class AppServiceProvider extends ServiceProvider
 
         if (config('filesystems.default') === 's3') {
             URL::forceRootUrl(config('filesystems.disks.s3.url'));
+        }
+
+        if (!app()->isLocal()) {
+            URL::formatHostUsing(function ($root, $path) {
+                if (Str::startsWith($path, 'storage/')) {
+                    $newPath = Str::replaceFirst('storage/', '', $path);
+                    return config('app.asset_url') . '/' . $newPath;
+                }
+                return $root;
+            });
         }
     }
 }
