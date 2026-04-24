9ecd0081916d6baafe79e1afa8e533309c847281
unknown <ismailbenmezi300@gmail.com>
2026-04-24 13:44:25 +0200

    add condition in method boot for fix url path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,17 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
         }
 
+        \Illuminate\Support\Facades\URL::formatPathUsing(function ($path) {
+            if (str_starts_with($path, 'storage/')) {
+                return str_replace('storage/', '', $path);
+            }
+            return $path;
+        });
+
+        if (config('app.asset_url')) {
+            \Illuminate\Support\Facades\URL::forceRootUrl(config('app.asset_url'));
+        }
+
         if (config('filesystems.default') === 's3') {
             URL::forceRootUrl(config('filesystems.disks.s3.url'));
         }
